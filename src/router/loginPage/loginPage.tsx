6bd2fb22f501dbf39ac6553c052e6fa27136bb6b
Muhammad Auzan Zaky Eryana <auzanzaky63@gmail.com>
2023-09-30 18:39:57 +0700

    selesai membuat loginPage

--- src/router/loginPage/loginPage.tsx
+++ src/router/loginPage/loginPage.tsx
@@ -33,68 +33,65 @@ export default function loginPage(props: PaperProps) {
   });
 
   return (
-    <Container
-     color="blue.7" pt="17rem">
-    <Paper radius="md" p="xl" withBorder {...props}>
-      <Text size="lg" fw={500}>
-        Welcome to Mantine, {type} with
-      </Text>
+    <div className="bg-[#f4f5f9] flex h-screen justify-center items-center">
+      <Paper mx="auto" w="25rem" radius="md" p="xl" withBorder {...props}>
+        <Text size="lg" fw={500}>
+          Login
+        </Text>
 
-      <Divider label="Or continue with email" labelPosition="center" my="lg" />
+        <Divider  labelPosition="center" my="lg" />
 
-      <form onSubmit={form.onSubmit(() => {})}>
-        <Stack>
-          {type === 'register' && (
+        <form onSubmit={form.onSubmit(() => {})}>
+          <Stack>
+            {type === 'register' && (
+              <TextInput
+                label="Name"
+                placeholder="Your name"
+                value={form.values.name}
+                onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+                radius="md"
+              />
+            )}
+
             <TextInput
-              label="Name"
-              placeholder="Your name"
-              value={form.values.name}
-              onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+              required
+              label="Email"
+              placeholder="Masukan email anda"
+              value={form.values.email}
+              onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+              error={form.errors.email && 'Invalid email'}
               radius="md"
             />
-          )}
 
-          <TextInput
-            required
-            label="Email"
-            placeholder="hello@mantine.dev"
-            value={form.values.email}
-            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
-            error={form.errors.email && 'Invalid email'}
-            radius="md"
-          />
-
-          <PasswordInput
-            required
-            label="Password"
-            placeholder="Your password"
-            value={form.values.password}
-            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
-            error={form.errors.password && 'Password should include at least 6 characters'}
-            radius="md"
-          />
-
-          {type === 'register' && (
-            <Checkbox
-              label="I accept terms and conditions"
-              checked={form.values.terms}
-              onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+            <PasswordInput
+              required
+              label="Password"
+              placeholder="Masukan kata sandi anda"
+              value={form.values.password}
+              onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+              error={form.errors.password && 'Password should include at least 6 characters'}
+              radius="md"
             />
-          )}
-        </Stack>
 
-        <Group justify="space-between" mt="xl">
-          <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
-            {type === 'register'
-              ? 'Already have an account? Login'
-              : "Don't have an account? Register"}
-          </Anchor>
-          <Button type="submit" radius="xl">
-            {upperFirst(type)}
-          </Button>
-        </Group>
-      </form>
-    </Paper>
-    </Container>
+            {type === 'register' && (
+              <Checkbox
+                label="I accept terms and conditions"
+                checked={form.values.terms}
+                onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+              />
+            )}
+          </Stack>
+
+          <Group justify="space-between" mt="xl">
+            <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
+              {type === 'register'
+                ? 'Already have an account? Login'
+                : "Tidak punya akun? buat disini"}
+            </Anchor>
+            <button className='rounded-md bg-[#4c62f0] text-sm text-white p-1 px-2'>Masuk</button>
+          </Group>
+        </form>
+      </Paper>
+    </div>
   );
 }
