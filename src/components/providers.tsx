4d66985b2d22a86b30a670e916c4c7530e9326e4
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2023-10-15 14:14:34 +0700

    `add`  react-cookie & svgr

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import {useState} from "react"
+import { useState } from "react";
 
 import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
+
+import { CookiesProvider } from "react-cookie";
 
 export default function UtilityProvider({
   children,
@@ -15,12 +17,15 @@ export default function UtilityProvider({
   children: React.ReactNode;
 }) {
   const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
-    <MantineProvider>
-      <BrowserRouter>{children}</BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </MantineProvider>
+      <CookiesProvider defaultSetOptions={{ path: "/" }}>
+        <MantineProvider>
+          <BrowserRouter>{children}</BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </MantineProvider>
+      </CookiesProvider>
     </QueryClientProvider>
   );
 }
